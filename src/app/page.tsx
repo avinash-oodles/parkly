4d8fdcccb2bdd50d74139a09ede1b9037d140ffe
avinash-oodles <avinash.kumarsingh@oodles.io>
2025-12-02 15:20:09 +0530

    setting font families

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-            Parly
-          </h1>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 ">
+      <h1 className=" text-3xl font-semibold ">
+        Parly
+      </h1>
+      <h2 className="text-2xl font-poppins font-semibold ">
+        This is a Poppins Heading
+      </h2>
     </div>
   );
 }
