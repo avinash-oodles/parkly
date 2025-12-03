bb954680818cb4d605dd02058ac7b12d7e93de3b
avinash-oodles <avinash.kumarsingh@oodles.io>
2025-12-03 10:49:04 +0530

    typography component setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-
+import Typography from "@/components/Typography/Typograpghy";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 ">
-      <h1 className=" text-3xl font-semibold ">
-        Parly
-      </h1>
-      <h2 className="text-2xl font-poppins font-semibold ">
-        This is a Poppins Heading
-      </h2>
+      <Typography variant="h1">
+        Welcome to Parkly
+      </Typography>
+      <Typography variant="body">
+        Welcome to Parkly
+      </Typography>
     </div>
   );
 }
